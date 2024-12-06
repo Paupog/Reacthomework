0b5f3a74732463abcb8fdf68d3aaf31c9e23d36a
Paupog <154589264+Paupog@users.noreply.github.com>
2024-12-06 14:28:47 +0100

    Created WeatherData.js

---
+++ WeatherData.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(null);
+
+  function showTemperature(response) {
+    setTemperature(response.data.main.temp);
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+  axios.get(url).then(showTemperature);
+}
